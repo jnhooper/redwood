8edad55c2da10035e8f312325e2cc9315c7c9ce7
John Hooper <nisse038@gmail.com>
2022-04-15 23:25:45 -0500

    workin through chapter 2

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,19 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <h2>AboutPage</h2>
+      <p>
+        Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>about</code>, link to me with `
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags title="Home" description="Home page" />
+      <h2>Home</h2>
+    </>
+  )
+}
+
+export default HomePage
